a6a17fbe1863f0de2b13d0fab901ca0c5089e3f7
Yanik dos Santos <yrsvntos@gmail.com>
2025-10-12 21:45:52 +0100

    add: adicionar à página de dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Container from "@/components/container";
+
+export default function Dashboard(){
+    return(
+        <Container>
+            <h1>Bem-vindo ao Dashboard</h1>    
+        </Container>
+    );
+}
