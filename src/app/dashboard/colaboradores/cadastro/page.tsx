b38d9a12c299ffe916344749dd00b94277b05657
Yanik dos Santos <yrsvntos@gmail.com>
2025-10-16 22:04:28 +0100

    refactor(pagina-cadastro): remover schema interno e importar userSchema do componente

--- src/app/dashboard/colaboradores/cadastro/page.tsx
+++ src/app/dashboard/colaboradores/cadastro/page.tsx
@@ -2,7 +2,7 @@
 
 import Container from "@/components/container";
 import { useForm } from "react-hook-form";
-import z from "zod";
+import { userSchema, FormData, optionsContrato, optionsEstado, optionsGenero } from "@/components/schema/users";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -15,41 +15,6 @@ import toast from "react-hot-toast";
 
 
 
-const userSchema = z.object({
-    id: z.union([z.string(), z.number()]).optional(),
-    nome: z.string().min(2, "O nome deve ter pelo menos 2 caracteres.").max(100, "O nome não pode ter mais de 100 caracteres."),
-    apelido: z.string().optional(),
-    email: z.string().email("Insira um e-mail válido.").max(120, "O e-mail não pode ultrapassar 120 caracteres."),
-    funcao: z.string().min(2, "Informe a função do colaborador."),
-    departamento: z.string().min(2, "Informe o departamento."),
-    contrato: z.enum(["Efetivo", "Temporário", "Estagiário"]).refine(
-        (val) => ["Efetivo", "Temporário", "Estagiário"].includes(val),
-        { message: "Tipo de contrato inválido." }
-    ),
-    estado: z.enum(["Ativo", "Inativo"]).refine(
-        (val) => ["Ativo", "Inativo"].includes(val),
-        { message: "Estado inválido." }
-    ),
-    genero: z.enum(["Masculino", "Feminino"]).refine(
-        (val) => ["Masculino", "Feminino"].includes(val),
-        { message: "Gênero inválido." }
-    ),
-    dataNascimento: z
-    .preprocess((val) => (typeof val === "string" ? new Date(val) : val), z.date({
-        error: "A data de nascimento é obrigatória.",
-    })),
-    telefone: z.string()
-    .regex(/^[0-9+\-\s()]+$/, "Número de telefone inválido.")
-    .optional(),
-    morada: z.string().optional(),
-    criadoEm: z.date().optional(),
-    atualizadoEm: z.date().optional(),
-})
-
-
-type FormData = z.infer<typeof userSchema>
-
-
 export default function Cadastro(){
     const router = useRouter();
     // Criar o formulário usando React Hook Form
@@ -58,9 +23,6 @@ export default function Cadastro(){
         mode: "onChange" // vai registar tudo sempre que o user modificar algo no input
     });
 
-    const optionsGenero = ["Masculino", "Feminino"];
-    const optionsEstado = ["Ativo", "Inativo"];
-    const optionsContrato = ["Efetivo", "Temporário", "Estagiário"];
 
     async function onSubmit(data: FormData){
         
