f35073ae48013fb25e01ce8f97cd09021388cff2
Yanik dos Santos <yrsvntos@gmail.com>
2025-10-17 19:45:08 +0100

    feat(dashboard): proteger rotas do dashboard com Firebase Auth e layout privado

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,13 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect, ReactNode } from "react";
+import { User } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { auth } from "@/services/firebaseConnection";
+import { onAuthStateChanged } from "firebase/auth";
 import Sidebar from "@/components/sidebar";
 import Header from "@/components/header";
+
+
 
 export default function DashboardLayout({
   children,
@@ -10,13 +16,34 @@ export default function DashboardLayout({
 }) {
   const [isOpen, setIsOpen] = useState(true); // estado do sidebar
 
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState<User | null>(null);
+
+  const router = useRouter();
+  useEffect(() => {
+    
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (!currentUser) {
+        router.push("/");
+      } else {
+        setUser(currentUser);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, [router]);
+
+  if (loading) return;
+  if (!user) return null; 
+
   return (
     <div className="flex min-h-screen bg-gray-100">
       <Sidebar isOpen={isOpen} toggle={() => setIsOpen(!isOpen)} />
 
       <div className="flex flex-col flex-1">
         <Header />
-        <main className="flex-1 p-6">{children}</main>
+        <main className="flex-1 p-6">{user && children}</main>
       </div>
     </div>
   );
