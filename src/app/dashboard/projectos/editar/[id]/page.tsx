bbfce05ec644880314e873fd876c70f3fc5e897b
Yanik dos Santos <yrsvntos@gmail.com>
2025-10-20 22:45:13 +0100

    feat(editar): adicionar a funcionalidade de editar os dados do projecto no banco de dados

---
+++ src/app/dashboard/projectos/editar/[id]/page.tsx
@@ -0,0 +1,12 @@
+
+import EditForm from "../components/editForm"
+
+interface EditPageProps {
+    params: Promise<{ id: string }>;
+}
+export default async function Editar({params}: EditPageProps){
+    const {id} = await params
+    return(
+        <EditForm id={id}/>
+    )
+}
