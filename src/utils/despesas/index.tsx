648396e0cb5bfa27dc808501339012d8489dbea7
Yanik dos Santos <yrsvntos@gmail.com>
2025-10-29 17:47:43 +0000

    feat(types): criar interface DespesaProps para tipagem de despesas

---
+++ src/utils/despesas/index.tsx
@@ -0,0 +1,17 @@
+export interface DespesaProps{
+    despesaId: string;
+    entidade: string;
+    valor: string;
+    descricao: string;
+    dataVencimento?: Date;
+    dataEmissao?: Date;
+    estado: "Pago" | "Pendente" | "Cancelada";
+    metodoPagamento: "Dinheiro" | "Transferência" | "Cartão";
+    criadoEm: Date;
+    atualizadoEm?: Date;
+    criadoPor: {
+        uid: string;
+        nome: string;
+    }
+
+}
