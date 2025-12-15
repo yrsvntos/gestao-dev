c595f97a2daf7895adfbacc5f213865f0a332d74
Yanik dos Santos <yrsvntos@gmail.com>
2025-12-15 20:33:28 +0000

    feat: adicionar a pasta de services

---
+++ src/services/firebaseAdmin.js
@@ -0,0 +1,12 @@
+// lib/firebaseAdmin.js
+import admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(
+      JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+    ),
+  });
+}
+
+export default admin;
